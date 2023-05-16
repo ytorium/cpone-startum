602312fe56cd5b2bdc69eee1c17bd8b18bfec877
ytorium <ytorium@gmail.com>
2023-05-16 21:18:49 +0300

    Commit 10

--- pages/twitter.js
+++ pages/twitter.js
@@ -1,25 +1,6 @@
 import { fetchUser } from '../services/twitter'
 
-function TwitterCard(props) {
-  let profileImageURL = props.twitterInfo.profileImageURL.replace(/_normal/g, "");
-  
-  return (
-    <div className="p-2 border rounded-md w-auto overflow-hidden">
-      <div className="relative">
-        <img className="mx-auto" src={props.twitterInfo.profileBannerURL} />
-        <div className="top-14 absolute left-4 lg:top-24 border-4 border-white rounded-full">
-          <img className="rounded-full h-24 w-24" src={profileImageURL} />
-        </div>
-      </div>
-      <div className="mt-28 lg:mt-16 px-4">
-        <p className="font-bold text-lg leading-6">{props.twitterInfo.name}</p>
-        <p className="text-sm text-gray-600">@{props.twitterInfo.screenName}</p>
-        <p className="font-bold text-lg leading-6">{props.twitterInfo.description}</p>
-        <p className="mt-4 text-sm whitespace-pre-wrap">{props.twitterInfo.userId}</p>
-      </div>
-    </div>
-  )
-}
+// mod A
 
 export default function Twitter(props) {
   return (
@@ -35,24 +16,4 @@ export default function Twitter(props) {
   )
 }
 
-export async function getServerSideProps(context) {
-  let twitterHandle = process.env.TEST_TWITTER_HANDLE
-  let twitterInfo = null
-
-  const userResponse = await fetchUser(twitterHandle)
-  twitterInfo = {
-    'screenName': userResponse.screen_name,
-    'name': userResponse.name,
-    'userId' : userResponse.user_id,
-    'description': userResponse.description,
-    'profileImageURL': userResponse.profile_image_url_https,
-    'profileBannerURL': userResponse.profile_banner_url,
-    'description': userResponse.description
-  }
-
-  return {
-    props: {
-      "twitterInfo": twitterInfo
-    },
-  }
-}
+// mod B
