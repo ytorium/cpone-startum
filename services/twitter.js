b4474159c842bb038e09829f8ca176033ec8b361
ytorium <ytorium@gmail.com>
2023-05-16 16:48:42 +0300

    Commit 9

--- services/twitter.js
+++ services/twitter.js
@@ -1,10 +1,24 @@
-import Twitter from 'twitter-lite';
+import { TwitterApi } from 'twitter-api-v2';
+
+//const twitterClient = new TwitterApi(process.env.TWITTER_BEARER_TOKEN);
+
+//const readOnlyClient = twitterClient.readOnly;
+
+//const user = async readOnlyClient.v2.userByUsername('YToimich');
+const screen_name = 'YToimich';
 
 export const fetchUser = async (screen_name) => {
-  const client = new Twitter({
-    version: "1.1",
-    bearer_token: process.env.TWITTER_BEARER_TOKEN
+  const client = new TwitterApi(process.env.TWITTER_BEARER_TOKEN);
+
+  return client.get('users/show', { screen_name: screen_name });
+}
+
+/*
+export const fetchUser = async (screen_name) => {
+  const client = new TwitterApi({
+     bearer_token: process.env.TWITTER_BEARER_TOKEN
   });
 
   return client.get('users/show', { screen_name: screen_name });
 }
+*/
