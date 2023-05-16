8b320be9dd83985df32ef41f679136346855af18
ytorium <ytorium@gmail.com>
2023-05-16 22:44:59 +0300

    Commit 13

--- pages/twitter.js
+++ pages/twitter.js
@@ -1,8 +1,22 @@
+import TwitterApi from 'twitter-api-v2';
 //import { fetchUser } from '../services/twitter'
 
 // mod A
 
 export default function Twitter() {
+/*
+const twitterClient = new TwitterApi({
+    appKey: process.env.APP_KEY!,
+    appSecret: process.env.APP_SECRET!,
+    accessToken: process.env.ACCESS_TOKEN!,
+    accessSecret: process.env.ACCESS_SECRET!,
+});
+*/
+  const twitterClient = new TwitterApi('6lzVZHTWWEaqocAewMyhNFdmL');
+  const readOnlyClient = twitterClient.readOnly;
+  const user = readOnlyClient.v2.userByUsername('YToimich');
+  console.log(user);
+
   return (
     <div className="p-2 max-w-xs lg:max-w-md mx-auto mt-12">
       <h2 className="text-2xl font-semibold">Twitter Card</h2>
